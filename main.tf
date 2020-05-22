adbd160028146f7e23cb9f5d3550088de9ad3509
Marius Tolzmann <m@rius.berlin>
2020-05-23 00:55:41 +0200

    Initial implementation

--- main.tf
+++ main.tf
@@ -3,3 +3,36 @@
 # And it continues with some lowercase information about the module
 # We might add more than one line for additional information
 # ------------------------------------------------------------------------------
+
+locals {
+  read_capacity  = var.billing_mode == "PROVISIONED" ? var.read_capacity : null
+  write_capacity = var.billing_mode == "PROVISIONED" ? var.write_capacity : null
+}
+
+resource "aws_dynamodb_table" "table" {
+  count = var.module_enabled ? 1 : 0
+
+  name     = var.name
+  hash_key = var.hash_key
+
+  billing_mode   = var.billing_mode
+  read_capacity  = local.read_capacity
+  write_capacity = local.write_capacity
+  tags           = var.tags
+
+  server_side_encryption {
+    enabled     = var.server_side_encryption
+    kms_key_arn = var.kms_key_arn
+  }
+
+  dynamic "attribute" {
+    for_each = var.attributes
+
+    content {
+      name = attribute.key
+      type = attribute.value
+    }
+  }
+
+  depends_on = [var.module_depends_on]
+}
